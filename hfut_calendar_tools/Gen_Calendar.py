6f117d3c2d3b397cc248cbaf1a94cbf78c9b9b0a
Hsinyuan Ping <thelastlin@users.noreply.github.com>
2019-08-16 23:36:47 +0800

    Merge class schedules for different lecturer at the same time.

--- hfut_calendar_tools/Gen_Calendar.py
+++ hfut_calendar_tools/Gen_Calendar.py
@@ -1,13 +1,22 @@
+from typing import List, Any
+
 from icalendar import Calendar, Event
 from uuid import uuid1
 import datetime
 
 
-def gen_calendar(class_dict: dict, schedule_list: list)->Calendar:
+def gen_calendar(class_dict: dict, schedule_list: list) -> Calendar:
     calendar = Calendar()
     calendar['version'] = '2.0'
     calendar['prodid'] = '-//HFUT//xCalendar//CN'
+    merged_schedule_list: List[dict] = []
     for lesson in schedule_list:
+        if (not merged_schedule_list) or (not(lesson["lessonId"] == merged_schedule_list[-1]["lessonId"] and lesson["date"] == merged_schedule_list[-1]["date"] and lesson[
+                'startTime'] == merged_schedule_list[-1]["startTime"])):
+            merged_schedule_list.append(lesson)
+        else:
+            merged_schedule_list[-1].update(personName=merged_schedule_list[-1]["personName"]+' ' + lesson["personName"])
+    for lesson in merged_schedule_list:
         event = Event()
         event.add("summary", class_dict[lesson["lessonId"]])
         event.add("description", lesson["personName"])
